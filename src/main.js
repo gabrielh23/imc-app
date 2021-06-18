b6e26aad73a69cd46725dd90e27783335ccb4595
Gabriel Henrique Gomes Firmino <gabriel.henrique.gf01@gmail.com>
2021-06-18 19:49:50 -0300

    imc-calculator
    Projeto finalizado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.mount('#app');
